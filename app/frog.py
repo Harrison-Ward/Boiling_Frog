b88b46421b71fc0335358ca2f714e8f21c8fd093
Harrison Ward <hward4116@gmail.com>
2023-06-02 19:13:52 -0700

    added code to create df for daily high scatter plots

--- app/frog.py
+++ app/frog.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import logging
 
 from decouple import config
+import matplotlib.pyplot as plt
 from meteostat import Daily
 from meteostat import Point
 from meteostat import units
+import numpy as np
 import pandas as pd
 import tweepy
 
@@ -24,9 +26,19 @@ try:
         access_token=access_token,
         access_token_secret=access_token_secret,
     )
-    logger.info('API credentials successfully verified.')
+    logger.info("API credentials successfully verified.")
 except Exception:
     logger.info("Failed to authenticate API credentials.")
+
+auth = tweepy.OAuth1UserHandler(
+       consumer_key,
+       consumer_secret,
+       access_token,
+       access_token_secret
+    )
+api = tweepy.API(auth)
+
+
 
 # fetch weather data from metostat API
 N = 30
@@ -42,7 +54,11 @@ data = data.convert(units.imperial)
 data = data.fetch()
 
 # Modify the pandas DF to assist in day of year pivot table
-data["month"], data["day"] = data.index.month, data.index.day
+data["year"], data["month"], data["day"] = (
+    data.index.year,
+    data.index.month,
+    data.index.day,
+)
 
 daily_max_avg = pd.DataFrame(data.tmax.groupby(by=[data.month, data.day]).mean())
 daily_max_max = pd.DataFrame(data.tmax.groupby(by=[data.month, data.day]).max())
@@ -53,9 +69,9 @@ daily_max_avg["time"] = daily_max_avg.index.values
 today = end.strftime("%Y-%m-%d")
 todays_high = data.tmax.loc[today]
 
-month, day = end.month, end.day
-todays_avg_high = daily_max_avg.tmax.loc[(month, day)]
-todays_max_high = daily_max_max.tmax.loc[(month, day)]
+Month, Day = end.month, end.day
+todays_avg_high = daily_max_avg.tmax.loc[(Month, Day)]
+todays_max_high = daily_max_max.tmax.loc[(Month, Day)]
 
 # Compare weather conditions
 if todays_high > todays_avg_high:
@@ -63,11 +79,43 @@ if todays_high > todays_avg_high:
 else:
     forecast_tweet = f"NYC: The high today is {todays_high:.1f}°F, which is {todays_high - todays_avg_high:.1f}°F cooler than today's {N}-year average."
 
-forecast_tweet += f"\n\nThe {N}-year historical high for today is {todays_max_high:.1f}°F."
+forecast_tweet += (
+    f"\n\nThe {N}-year historical high for today is {todays_max_high:.1f}°F."
+)
+
+# generate dataframe for scatter plot
+pd.options.mode.chained_assignment = None
+daily_hist_series = data[(data["month"] == Month) & (data["day"] == Day)]
+daily_hist_series["most_recent"] = np.where(
+    daily_hist_series["year"] == daily_hist_series["year"].max(), 1, 0
+)
+
+# plot the weather data
+plt.style.use("fivethirtyeight")
+plt.scatter(
+    daily_hist_series["year"][daily_hist_series["most_recent"] == 0],
+    daily_hist_series["tmax"][daily_hist_series["most_recent"] == 0],
+    color="gray",
+)
+plt.scatter(
+    daily_hist_series["year"][daily_hist_series["most_recent"] == 1],
+    daily_hist_series["tmax"][daily_hist_series["most_recent"] == 1],
+    color="red",
+    label="Today",
+)
+plt.xlabel("Year")
+plt.ylabel("Daily High")
+plt.title("Daily High by Year")
+plt.legend(loc="upper left")
+plt.savefig('daily_plot.jpeg')
+
+media = api.media_upload(filename="daily_plot.jpeg")
 
 # tweet the takeaway
-response = client.create_tweet(text=forecast_tweet)
+response = client.create_tweet(text=forecast_tweet, media_ids=[media.media_id_string])
 logger.info(
     f"Today's forecast tweeted: https://twitter.com/user/status/{response.data['id']}"
 )
-print(f"Today's forecast tweeted: https://twitter.com/user/status/{response.data['id']}")
+print(
+    f"Today's forecast tweeted: https://twitter.com/user/status/{response.data['id']}"
+)
